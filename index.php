3bbd58ac2124a76f46a7f8c37f6fbeac6ce9010f
Daniella Saadon <saadondaniella@gmail.com>
2025-10-03 13:57:44 +0200

    uppdaterat index

--- index.php
+++ index.php
@@ -3,29 +3,25 @@ require __DIR__.'/data.php';
 require __DIR__.'/header.php';
 ?>
 
-<br>
 <div class="site-header">
+<h1>Let's keep up with some fotball teams</h1> 
 
-  <h1><?php echo 'Lets keep up with some fotball teams';?></h1> 
-  <h2 id="firstpart"><?php echo 'Who is the champions?';?></h2>
-  <p><?php echo 'Lets rank them all';?></p>
+  <h2 id="firstpart">Who is the champions?</h2>
+  <p>Lets rank them all</p>
 
 </div>
-<br>
 
 <div class="teams-grid">
 
   <?php foreach ($teams as $name => $info):?>
+
   <div class="team-box">
-    
+    <span class="uefa">UEFA ranking: <?= $info['uefa-coefficient-ranking'] ?></span>
     <img src="<?= $info['logo'] ?>" alt="<?= $name ?> logo" class="team-logo">
     <h2><?= $name?></h2>
     <h3>League: <?= $info['league'] ?></h3>
-    <h3>City: <?= $info['city'] ?></h3>
-  
-    <h3 id="uefa">UEFA ranking: <?= $info['uefa-coefficient-ranking'] ?></h3>
-  
-  <a href="<?= $info['url'] ?>" target="_blank">Official website</a>
+    <h3>City: <?= $info['city'] ?></h3> 
+    <a href="<?= $info['url'] ?>" target="_blank">Official website</a>
   
   <h3>Opponents:</h3>
 
