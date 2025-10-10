393eb275b6fd6b12d53cdb8541086400b5ebdd84
Daniella Saadon <saadondaniella@gmail.com>
2025-10-10 14:25:45 +0200

    uppdaterat data filen

--- data.php
+++ data.php
@@ -92,7 +92,7 @@ $teams = [
         ]
     ],
     'Benfica' => [
-        'league' => 'campeonato nacional de futebol feminino (Portugal)',
+        'league' => 'Campeonato nacional de futebol feminino (Portugal)',
         'uefa-coefficient-ranking' => 11,
         'city' => 'Lissabon',
         'url' => 'https://www.slbenfica.pt/en-us/futebol/feminino',
