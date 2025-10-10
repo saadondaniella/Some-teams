a6f8f4039bc3dd078499985a2e937a5c2689dd3f
Daniella Saadon <saadondaniella@gmail.com>
2025-10-10 14:26:29 +0200

    uppdaterat i headern

--- header.php
+++ header.php
@@ -4,23 +4,22 @@
 <meta charset="UTF-8">
 <title>Some Teams</title>
 
-<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta property="og:title" content="Worlds greates webpage for every fotball lover">
 <meta property="og:description" content="Discover the score and ranking of fotball.">
 
 <link rel="stylesheet" href="style.css">
+</head>
 
-  </head>
   <body>
 
   <nav>
-       <a href="index.html" class="logo">
-    <img src="1.png" alt="Logotyp" height="80px" width="80px">
-  </a>
-        <a href="index.php">Home</a>
-        <a href="about.php">About</a>
-        <a href="team.php">Your favorit team</a>
-      
-  </nav>
+    <a href="index.html" class="logo">
+    <img src="1.png" alt="Logotyp" height="80px" width="80px"></a>
 
-<!--  html dokumentet -->
+    <a href="index.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="team.php">Your favorite team</a>
+
+  </nav>
+  
